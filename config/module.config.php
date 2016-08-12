76bb253b245b04e1d0c9013c38d690c5b92c0018
John L <jlew@taurus-v>
2016-08-12 17:54:46 -0400

    removed array brackets in module config file to support < php-5.4 if needed

--- config/module.config.php
+++ config/module.config.php
@@ -5,10 +5,10 @@ namespace LpiAssetLoader;
 use LpiAssetLoader\Model\AssetLoader;
 use LpiAssetLoader\Model\AssetLoaderFactory;
 
-return[
-   'service_manager' => [
-       'factories' => [
-           'LpiAssetLoader\Model\AssetLoader' => 'LpiAssetLoader\Model\AssetLoaderFactory'
-       ]
-   ]
-];
+return array(
+   'service_manager' => array(
+      'factories' => array(
+         'LpiAssetLoader\Model\AssetLoader' => 'LpiAssetLoader\Model\AssetLoaderFactory'
+      )
+   )
+);
